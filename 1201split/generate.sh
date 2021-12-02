4ab5338c085baae2fb50cc8ccbffe67a457e34cf
catcodeme <1020082805@qq.com>
2021-12-02 18:53:54 +0800

    1202

--- 1201split/generate.sh
+++ 1201split/generate.sh
@@ -2,22 +2,22 @@
 
 set -e
 
-# python3 lac_full_text.py ../docs/chq/bao_lian_deng.txt chq bao_lian_deng
-# python3 lac_full_text.py ../docs/chq/bo_ya*.txt chq bo_ya
-# python3 lac_full_text.py ../docs/chq/gong_gong*.txt chq gong_gong
-# python3 lac_full_text.py ../docs/chq/hua_*.txt chq hua_mu_lan
-# python3 lac_full_text.py ../docs/chq/jiao_*.txt chq jiao_zhong
-# python3 lac_full_text.py ../docs/chq/li_wa*.txt chq li_wa
-# python3 lac_full_text.py ../docs/chq/nv_*.txt chq nv_wa_de_gu_shi
-# python3 lac_full_text.py ../docs/chq/qin_*.txt chq qin_shi
-# python3 lac_full_text.py ../docs/chq/wo_*.txt chq wo_xin
+# python3 lac_full_text.py ../docs/chq/bao_lian_deng.txt chq bao_lian_deng /home/hulj/workspace/lg/a
+# python3 lac_full_text.py ../docs/chq/bo_ya*.txt chq bo_ya /home/hulj/workspace/lg/all_unique
+python3 lac_full_text.py ../docs/chq/gong_gong*.txt chq gong_gong /home/hulj/workspace/lg/all_unique
+python3 lac_full_text.py ../docs/chq/hua_*.txt chq hua_mu_lan /home/hulj/workspace/lg/all_unique
+python3 lac_full_text.py ../docs/chq/jiao_*.txt chq jiao_zhong /home/hulj/workspace/lg/all_unique
+python3 lac_full_text.py ../docs/chq/li_wa*.txt chq li_wa /home/hulj/workspace/lg/all_unique
+python3 lac_full_text.py ../docs/chq/nv_*.txt chq nv_wa_de_gu_shi /home/hulj/workspace/lg/all_unique
+python3 lac_full_text.py ../docs/chq/qin_*.txt chq qin_shi /home/hulj/workspace/lg/all_unique
+python3 lac_full_text.py ../docs/chq/wo_*.txt chq wo_xin /home/hulj/workspace/lg/all_unique
 
-python3 lac_full_text.py ../docs/hyf/cuo_*.txt hyf cuo_cuo_cuo
-python3 lac_full_text.py ../docs/hyf/di_san_*.txt hyf di_san_zhi
-python3 lac_full_text.py ../docs/hyf/dian_nao.txt hyf dian_nao
-python3 lac_full_text.py ../docs/hyf/hua_*.txt hyf hua_pi
-python3 lac_full_text.py ../docs/hyf/liang_*.txt hyf liang_ge_hai_zi
-python3 lac_full_text.py ../docs/hyf/liu_*.txt hyf liu_zai_zhong
-python3 lac_full_text.py ../docs/hyf/qing_*.txt hyf qing_feng
-python3 lac_full_text.py ../docs/hyf/ru_*.txt hyf ru_guo
-python3 lac_full_text.py ../docs/hyf/wo_*.txt hyf wo_ke
+# python3 lac_full_text.py ../docs/hyf/cuo_*.txt hyf cuo_cuo_cuo
+# python3 lac_full_text.py ../docs/hyf/di_san_*.txt hyf di_san_zhi
+# python3 lac_full_text.py ../docs/hyf/dian_nao.txt hyf dian_nao
+# python3 lac_full_text.py ../docs/hyf/hua_*.txt hyf hua_pi
+# python3 lac_full_text.py ../docs/hyf/liang_*.txt hyf liang_ge_hai_zi
+# python3 lac_full_text.py ../docs/hyf/liu_*.txt hyf liu_zai_zhong
+# python3 lac_full_text.py ../docs/hyf/qing_*.txt hyf qing_feng
+# python3 lac_full_text.py ../docs/hyf/ru_*.txt hyf ru_guo
+# python3 lac_full_text.py ../docs/hyf/wo_*.txt hyf wo_ke
--- 1201split/lac_full_text.py
+++ 1201split/lac_full_text.py
@@ -7,12 +7,14 @@ en = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N',
       'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
 
 
-def doSplit(_filePath, _resultDir, _resultFileName):
+def doSplit(_filePath, _resultDir, _resultFileName, _wordInfo):
     # 带词性统计
     lac = LAC(mode="lac")
     # lac.load_customization('all_unique', sep=None)
 
     lineListWithSuffix = []
+
+    _contentWordList = _wordInfo['result']
     with open(_filePath, 'r') as f:
         fls = f.readlines()
         # result = lac.run(fls)
@@ -27,24 +29,67 @@ def doSplit(_filePath, _resultDir, _resultFileName):
                 if type == 'w' or type.isdigit() or type in en:
                     _lineStr += wordList[j]
                 else:
-                    _lineStr += wordList[j] + "/"
+                    if wordList[j] in _contentWordList:
+                        _lineStr += wordList[j] + "/"
+                    else:
+                        _lineStr += "【" + wordList[j] + "】"
+
             lineListWithSuffix.append(_lineStr)
 
     if not os.path.exists('1201split/'+_resultDir):
         os.makedirs('1201split/'+_resultDir)
 
-    with open('1201split/'+resultDir+'/'+resultFileName, 'w') as f:
+    with open('1201split/'+resultDir+'/' + _resultFileName, 'w') as f:
         for i, val in enumerate(lineListWithSuffix):
             f.write(val+'\n')
+
+
+def loadWords(_filename):
+    type = ""
+    src_count = 0
+    real_count = 0
+    result = []
+    with open(_filename, 'r') as f:
+        fls = f.readlines()
+        for i in range(0, len(fls)):
+            _tmp = fls[i].rstrip('\n')
+            if (i == 0):
+                type += _tmp
+                continue
+            if (i == 1):
+                src_count = _tmp
+                continue
+            if _tmp in en:
+                continue
+            real_count += 1
+            result.append(_tmp)
+    name = {
+        "a": "甲级词",
+        "b": "乙级词",
+        "c": "丙级词",
+        "d": "丁级词"
+    }
+
+    return {
+        "type": type,
+        "src_count": src_count,
+        "real_count": real_count,
+        "result": result
+        # "zh_name": name[_filename]
+    }
 
 
 if __name__ == '__main__':
     srcFile = sys.argv[1]
     resultDir = sys.argv[2]
     resultFileName = sys.argv[3]
+    contentFile = sys.argv[4]
+
     # _statFilePrefix = sys.argv[3]
     # _splitInfo = doSplit(srcFile, contentFile)
     # _wordInfo = loadWords(contentFile)
     # doStatistic(_splitInfo, _wordInfo, _statFilePrefix)
-    doSplit(srcFile, resultDir, resultFileName)
+    _wordInfo = loadWords(contentFile)
+    # print(_wordInfo)
+    doSplit(srcFile, resultDir, resultFileName, _wordInfo)
     print("success")
